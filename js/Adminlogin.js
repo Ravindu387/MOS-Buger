5d7a2e5b74dd79eb866e9e50d0c4eccc562ecd16
Ravindu387 <ravindunim6121@gmail.com>
2024-10-01 19:01:34 +0530

    .

--- js/Adminlogin.js
+++ js/Adminlogin.js
@@ -3,7 +3,7 @@ function login(){
     let password=document.getElementById('password').value;
     if (username=="M001" && password==223) {
         window.open('../html/Mprofile.html', '_blank'); 
-          console.log("hello")
+        //   console.log("hello")
           
     }
 }
